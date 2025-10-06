c0ae0ebd2df6bfe24ec7ab85bae6aa838c278474
leijessica11 <leijessica11@gmail.com>
2025-10-06 00:59:56 -0400

    Forgot to generate a token using the user's ID

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -46,6 +46,7 @@ router.post("/register", async (req, res) =>{
         })
 
         await user.save();
+        const token = generateToken(user._id);
 
         res.status(201).json({
             token,
